830c81584251e07bf51ed12efd82c44b1d846762
taren07 <taro.enchi@gmail.com>
2024-06-13 23:57:24 +0900

    カテゴリ、金額、メモの追加

--- frontend/src/routes/input/index.tsx
+++ frontend/src/routes/input/index.tsx
@@ -1,6 +1,8 @@
 import { component$ } from "@builder.io/qwik";
 import { Form } from "@builder.io/qwik-city";
 
+
+
 export default component$(() => {
   return (
     <div class="flex items-center justify-center">
@@ -48,6 +50,31 @@ export default component$(() => {
               <h5>日付</h5>
               <input
                 type="date"
+                class="w-full rounded-md border-gray-200 text-gray-900 focus:ring-blue-500"
+              />
+            </div>
+
+            <div class="grid justify-items-start space-y-2">
+              <h5>カテゴリ</h5>
+              <select class="w-full rounded-md border-gray-200 text-gray-900 focus:ring-blue-500">
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
+              </select>
+            </div>
+
+            <div class="grid justify-items-start space-y-2">
+              <h5>金額</h5>
+              <input
+                type="number"
+                class="w-full rounded-md border-gray-200 text-gray-900 focus:ring-blue-500"
+              />
+            </div>
+
+            <div class="grid justify-items-start space-y-2">
+              <h5>メモ</h5>
+              <input
+                type="text"
                 class="w-full rounded-md border-gray-200 text-gray-900 focus:ring-blue-500"
               />
             </div>
